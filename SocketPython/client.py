ebd9ad750dffaa867c2c84335a7fc01977c3857e
carenferreira <49703778+carenferreira@users.noreply.github.com>
2023-08-26 13:46:21 -0300

    Add files via upload

---
+++ SocketPython/client.py
@@ -0,0 +1,23 @@
+import socket
+import json
+
+HOST = '127.0.0.1'
+PORT = 5060
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((HOST, PORT))
+
+dados = []
+
+while True:
+    data = client.recv(1024)
+
+    if not data:
+        break
+
+    dados.append(data)
+
+print(dados[-1].decode())
+
+client.close()
