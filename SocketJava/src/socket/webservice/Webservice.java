5c6068de9cfbe4bffd4ea6decc481a1d472778ec
carenferreira <49703778+carenferreira@users.noreply.github.com>
2023-09-04 14:22:50 -0300

    varsao json

--- SocketJava/src/socket/webservice/Webservice.java
+++ SocketJava/src/socket/webservice/Webservice.java
@@ -1,33 +1,24 @@
 package socket.webservice;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.simple.*;
 
 public class Webservice {
 	public static void main(String[] args) {
-		Aluno[] alunosArray1 = {
-		new Aluno("5000", "1", 21),
-		new Aluno("5001", "2", 21),
-		new Aluno("5002", "3", 20),
-		new Aluno("5003", "4", 20),
-		new Aluno("5004", "5", 27),
-		};
-		Aluno[] alunosArray2 = {
-		new Aluno("3000", "1", 20),
-		new Aluno("3001", "2", 30),
-		new Aluno("3002", "3", 20),
-		new Aluno("3003", "4", 22),
-		new Aluno("3004", "5", 20),
-		};
-
-		Turma webservices = new Turma("1", "WebServices", 2023, alunosArray1);
-		Turma too = new Turma("2", "TOO", 2023, alunosArray2);
+		List<Turma> turmas = new ArrayList<>();
 		
-		System.out.println(webservices);
+		criarTurmas(turmas);
+		String stringTurmasJson = converteTurmasJson(turmas);
+		
+		System.out.println(stringTurmasJson);
 		
 		final int PORT = 5060;
 
@@ -39,8 +30,16 @@ public class Webservice {
 				Socket client = server.accept();
 				System.out.println("Cliente conectado: " + client.getInetAddress().getHostAddress());
 				PrintWriter saida = new PrintWriter(client.getOutputStream(), true);
-				saida.println(webservices.toString() + "\n" + too.toString());
+				saida.println(stringTurmasJson);
+				saida.println();
+				
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				String lideres = in.readLine();
+				
+				System.out.println(lideres);
+				
 				saida.close();
+				in.close();
 				client.close();
 			}
 
@@ -48,5 +47,53 @@ public class Webservice {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private static String converteTurmasJson(List<Turma> turmas) {
+		JSONArray turmasJson = new JSONArray();
+
+		for(Turma turma : turmas) {
+			JSONObject turmaJson = new JSONObject();
+			turmaJson.put("id", turma.getId());
+			turmaJson.put("nome", turma.getNome());
+			turmaJson.put("ano", String.valueOf(turma.getAno()));
+
+
+			JSONArray alunos = new JSONArray();
+
+			for(Aluno a : turma.getArrayAlunos()) {
+				JSONObject aluno = new JSONObject();
+				aluno.put("matricula", a.getMatricula());
+				aluno.put("nome", a.getNome());
+				aluno.put("idade", a.getIdade());
+				alunos.add(aluno);
+			}
+
+			turmaJson.put("alunos", alunos);
+			turmasJson.add(turmaJson);
+		}
+
+		return turmasJson.toJSONString();
+	}
+
+	private static void criarTurmas(List<Turma> turmas) {
+		Aluno[] alunosArray1 = {
+				new Aluno("5000", "1", 21),
+				new Aluno("5001", "2", 21),
+				new Aluno("5002", "3", 20),
+				new Aluno("5003", "4", 20),
+				new Aluno("5004", "5", 27),
+		};
+		Aluno[] alunosArray2 = {
+				new Aluno("3000", "1", 20),
+				new Aluno("3001", "2", 30),
+				new Aluno("3002", "3", 20),
+				new Aluno("3003", "4", 22),
+				new Aluno("3004", "5", 20),
+		};
+
+		turmas.add(new Turma("1", "WebServices", 2023, alunosArray1));
+		turmas.add(new Turma("2", "TOO", 2023, alunosArray2));
+
 	}
 }
